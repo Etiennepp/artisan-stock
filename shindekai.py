f564c3a001b8d1c8a6b5bb555d55eda2a528fb02
Etiennepp <etiennep09@hotmail.fr>
2024-01-16 13:27:45 +0100

    Added plyer for notifications

--- shindekai.py
+++ shindekai.py
@@ -1,24 +1,32 @@
-import os
 import urllib.request
 import time
 import re
+from plyer import notification
+
 
 url = "https://www.artisan-jp.com/fx-shidenkai-eng.html"
-check_interval = 60 * 1
-size = "M"
+check_interval = 1 * 1
+size = "L"
+
 regex = f"<li type=\"text\" name=\"size\"[\s\w\=\";#:\(\),]*>{size}</li>"
 
 def notify(text):
-    os.system("""
-              osascript -e 'display notification "{}" with title "NJ FX Shindekai V2"'
-              """.format(text))
+	
+	try:
+		notification.notify(
+			title = 'NJ FX Shindekai V2',
+			message = text,
+			app_icon = None,
+			timeout = 10,
+		)
+	except:
+		return None
 
 while True:
 	try:
 		page = urllib.request.urlopen(url)
 		content = str(page.read())
 		result = re.search(regex, content)
-		print(result.group())
 		if result:
 			tag = result.group()
 			if not "cursor:none" in tag:
