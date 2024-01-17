3398ca015519f00a763beaeff5eb0ce1a9a4f523
Etiennepp <etiennep09@hotmail.fr>
2024-01-17 20:15:12 +0100

    modified:   params.py

--- params.py
+++ params.py
@@ -1,2 +1,2 @@
-check_interval = 60 * 5 # Interval (in seconds) between each availability check
+check_interval = 60 * 10 # Interval (in seconds) between each availability check
 api_url = "https://www.artisan-jp.com/get_syouhin.php"
